4440dcb1b80d186bb6241ff12b3d42584a7a4cfb
giftedtech <103584295+Giftedmaurice@users.noreply.github.com>
2024-02-17 15:38:25 +0300

    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ
    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ

--- pages/index.vue
+++ pages/index.vue
@@ -53,7 +53,7 @@
 <template>
 	<div class="max-w-xl mx-auto text-black">
 		<a
-			href="https://vercel.com/templates/next.js/blob-sveltekit"
+			href="https://giftedai.vercel.app"
 			class="flex justify-center px-10 py-2 mx-auto space-x-1 text-sm font-medium text-center text-gray-600 transition-all rounded-full shadow-sm group bg-white/30 ring-1 ring-gray-900/5 hover:shadow-lg active:shadow-sm"
 		>
 			Use for Free without Login. Powered by GiftedTech
